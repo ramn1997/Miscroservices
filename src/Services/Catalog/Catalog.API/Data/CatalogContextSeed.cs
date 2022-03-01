1bab471af78303484dec459531b212b99dcaf8be
ramn1997 <ramnj94907@gmail.com>
2022-03-01 16:24:23 +0530

    New Updates

--- src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -25,7 +25,7 @@ namespace Catalog.API.Data
                     Id = "602d2149e773f2a3990b47f5",
                     Name = "IPhone X",
                     Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    Description = "The iPhone X contains Apple's A11 Bionic system-on-chip, also used in the iPhone 8 and 8 Plus, which is a six-core processor with two cores optimized for performance (25% faster than the A10 Fusion processor), along with four cores optimized for efficiency (70% faster than the previous generation).",
                     ImageFile = "product-1.png",
                     Price = 950.00M,
                     Category = "Smart Phone"
@@ -33,9 +33,9 @@ namespace Catalog.API.Data
                 new Product()
                 {
                     Id = "602d2149e773f2a3990b47f6",
-                    Name = "Samsung 10",
+                    Name = "Samsung S10",
                     Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    Description = "Galaxy S10 by Samsung is amongst the new smartphones that was launched alongside the Samsung Galaxy S10+ and the Samsung Galaxy S10E. The Samsung Galaxy S10 has a hole-punch design at the front. This lightweight and easy to carry phone weighs around 157 grams and measures 149.9 mm x 70.4 mm x 7.8 mm." ,
                     ImageFile = "product-2.png",
                     Price = 840.00M,
                     Category = "Smart Phone"
@@ -43,9 +43,9 @@ namespace Catalog.API.Data
                 new Product()
                 {
                     Id = "602d2149e773f2a3990b47f7",
-                    Name = "Huawei Plus",
+                    Name = "Huawei P10 Plus",
                     Summary = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    Description = "The phone comes with a 5.50-inch touchscreen display with a resolution of 1440x2560 pixels. Huawei P10 Plus is powered by an octa-core HiSilicon Kirin 960 processor. It comes with 6GB of RAM. The Huawei P10 Plus runs Android 7.0 and is powered by a 3750mAh non-removable battery.",
                     ImageFile = "product-3.png",
                     Price = 650.00M,
                     Category = "White Appliances"
